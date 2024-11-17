f192c39b01434221bd59b8e79aefb32306d9c949
Vivian Sandler <161636280+VivianSandler@users.noreply.github.com>
2024-11-17 21:38:06 +0000

    Created forEach function to display data from data.js in index.html

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+import { lakes } from "./data.js";
+
+const lakesList = document.getElementsByClassName("lakes-list")[0];
+
+lakes.forEach((lake) => {
+  let lakePara = document.createElement("p");
+  let lakeParaNameText = document.createTextNode(lake.name);
+  lakePara.appendChild(lakeParaNameText);
+  lakesList.appendChild(lakePara);
+  let lakeImg = document.createElement("img");
+  lakeImg.src = lake.img;
+  lakesList.appendChild(lakeImg);
+  let lakeAbout = document.createElement("p");
+  let lakeAboutText = document.createTextNode(lake.description);
+  lakeAbout.appendChild(lakeAboutText);
+  lakesList.appendChild(lakeAbout);
+});
 

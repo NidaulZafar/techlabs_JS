322952a476edc2a8a89a343dc3dc1c8312852d6e
Vivian Sandler <vivianmargothsandler@gmail.com>
2024-11-17 20:24:17 +0000

    Add to the forEach method to include img and description of each lake in our HTML

--- TechLabs Bootcamp/techlabs_JS/script.js
+++ TechLabs Bootcamp/techlabs_JS/script.js
@@ -4,7 +4,14 @@ const lakesList = document.getElementsByClassName("lakes-list")[0];
 
 lakes.forEach((lake) => {
   let lakePara = document.createElement("p");
-  let lakeParaText = document.createTextNode(lake.name);
-  lakePara.appendChild(lakeParaText);
+  let lakeParaNameText = document.createTextNode(lake.name);
+  lakePara.appendChild(lakeParaNameText);
   lakesList.appendChild(lakePara);
+  let lakeImg = document.createElement("img");
+  lakeImg.src = lake.img;
+  lakesList.appendChild(lakeImg);
+  let lakeAbout = document.createElement("p");
+  let lakeAboutText = document.createTextNode(lake.description);
+  lakeAbout.appendChild(lakeAboutText);
+  lakesList.appendChild(lakeAbout);
 });

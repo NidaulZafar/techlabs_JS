350fa66272028fc3bc08c812bbc6f43bd7e0ebf5
Vivian Sandler <vivianmargothsandler@gmail.com>
2024-11-17 19:14:55 +0000

    Add forEach loop using data imported from data.js

---
+++ TechLabs Bootcamp/techlabs_JS/script.js
@@ -0,0 +1,10 @@
+import { lakes } from "data.js";
+
+const lakesList = document.getElementsByClassName("lakes-list")[0];
+
+lakes.forEach((lake) => {
+  let lakePara = document.createElement("p");
+  let lakeParaText = document.createTextNode(lake.name);
+  lakePara.appendChild(lakeParaText);
+  lakesList.appendChild(lakePara);
+});
